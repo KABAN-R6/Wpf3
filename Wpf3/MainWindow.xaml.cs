460c2bf5faac9224cd84011212e031c1f06af769
is13126 <is13126@B206-2>
2023-03-20 11:57:14 +0700

    Добавьте файлы проекта.

---
+++ Wpf3/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace Wpf3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ObservableCollection<string> taskes;
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            taskes = new ObservableCollection<string> { "fsafas" };
+            list1.ItemsSource = taskes;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string task = tx1.Text;
+            taskes.Add(task);
+            
+        }
+
+        private void task1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void list1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (list1.TabIndex == list1.SelectedIndex)
+               
+                list1.SelectedIndex = list1.SelectedIndex;
+
+            
+            for (int x = list1.SelectedIndex - 1; x >= 0; x--)
+            {
+                list1.Items.Remove(x);
+            }
+        }
+    }
+}
---
+++ Wpf3/task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf3
+{
+    class task
+    {
+        public string Name { get; set; } 
+        public string time { get; set; }  
+        public override string ToString() => $"{Name} ({time})";
+    }
+}
