e25e0782645e9bfd85b610b3e06cc5a766141389
is13126 <is13126@B206-2>
2023-03-23 08:43:20 +0700

    dsgdsgsdgsdg

--- Wpf3/MainWindow.xaml.cs
+++ Wpf3/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
-using System.Collections.ObjectModel;
 
 namespace Wpf3
 {
@@ -23,21 +10,21 @@ namespace Wpf3
     public partial class MainWindow : Window
     {
 
-        ObservableCollection<string> taskes;
+
 
         public MainWindow()
         {
-            
+
             InitializeComponent();
-            taskes = new ObservableCollection<string> { "fsafas" };
-            list1.ItemsSource = taskes;
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string task = tx1.Text;
-            taskes.Add(task);
-            
+            list1.Items.Add(new task { Name = tx1.Text, time = "12" });
+            list1.Items.Refresh();
+
         }
 
         private void task1_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -47,26 +34,23 @@ namespace Wpf3
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void list1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (list1.TabIndex == list1.SelectedIndex)
-               
-                list1.SelectedIndex = list1.SelectedIndex;
 
-            
-            for (int x = list1.SelectedIndex - 1; x >= 0; x--)
-            {
-                list1.Items.RemoveAt(x);
-            }
-            ///https://metanit.com/sharp/wpf/5.6.php
-            ///
+
+            var firstPerson = list1.Items[list1.SelectedIndex];   // получаем первый объект
+            list1.Items.Remove(firstPerson);
 
 
 
 
+        }
+
+        private void list1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
 
         }
     }
--- Wpf3/task.cs
+++ Wpf3/task.cs
@@ -8,8 +8,12 @@ namespace Wpf3
 {
     class task
     {
-        public string Name { get; set; } 
-        public string time { get; set; }  
-        public override string ToString() => $"{Name} ({time})";
+        public string Name { get; set; } = "";
+        public string time { get; set; }
+        //public override string ToString() => $"{Name} ({time})";
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {time}";
+        }
     }
 }
